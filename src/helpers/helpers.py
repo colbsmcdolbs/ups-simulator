ddbc377f9604dd4a66fbbc6a854e52499a415611
Colby Allen <colbyjayallen@gmail.com>
2020-10-13 20:26:33 -0600

    Working.... kinda

--- src/helpers/helpers.py
+++ src/helpers/helpers.py
@@ -2,8 +2,10 @@ from classes.truck import Truck
 from classes.package_table import Package_Table
 
 def generate_truck(truck_number: int, package_table: Package_Table) -> Truck:
+    """
+    Manually load ALL the trucks!
+    """
     truck = Truck()
-    
     if truck_number is 1:
         truck.add_package(package_table.get(13))
         truck.add_package(package_table.get(14))
@@ -12,38 +14,43 @@ def generate_truck(truck_number: int, package_table: Package_Table) -> Truck:
         truck.add_package(package_table.get(19))
         truck.add_package(package_table.get(20))
         truck.add_package(package_table.get(1))
-        truck.add_package(package_table.get())
-        truck.add_package(package_table.get())
-        truck.add_package(package_table.get())
-        truck.add_package(package_table.get())
-        truck.add_package(package_table.get())
-        truck.add_package(package_table.get())
-        truck.add_package(package_table.get())
-        truck.add_package(package_table.get())
-        truck.add_package(package_table.get())
+        truck.add_package(package_table.get(34))
+        truck.add_package(package_table.get(21))
+        truck.add_package(package_table.get(7))
+        truck.add_package(package_table.get(29))
+        truck.add_package(package_table.get(37))
+        truck.add_package(package_table.get(30))
+        truck.add_package(package_table.get(39))
+        truck.add_package(package_table.get(27))
+        truck.add_package(package_table.get(35))
     elif truck_number is 2:
-        truck.add_package(package_table.get(3))
+        truck.add_package(package_table.get(25))
         truck.add_package(package_table.get(18))
         truck.add_package(package_table.get(36))
-        truck.add_package(package_table.get(38))
-        truck.add_package(package_table.get())
-        truck.add_package(package_table.get())
-        truck.add_package(package_table.get())
-        truck.add_package(package_table.get())
-        truck.add_package(package_table.get())
-        truck.add_package(package_table.get())
-        truck.add_package(package_table.get())
-        truck.add_package(package_table.get())
-        truck.add_package(package_table.get())
-        truck.add_package(package_table.get())
-        truck.add_package(package_table.get())
-        truck.add_package(package_table.get())
-    elif truck_number is 3:
+        truck.add_package(package_table.get(26))
         truck.add_package(package_table.get(6))
-        truck.add_package(package_table.get(25))
+        truck.add_package(package_table.get(22))
+        truck.add_package(package_table.get(24))
         truck.add_package(package_table.get(28))
+        truck.add_package(package_table.get(4))
+        truck.add_package(package_table.get(40))
+        truck.add_package(package_table.get(31))
         truck.add_package(package_table.get(32))
+        truck.add_package(package_table.get(17))
+        truck.add_package(package_table.get(12))
+        truck.add_package(package_table.get(23))
+        truck.add_package(package_table.get(11))
+    elif truck_number is 3:
+        truck.add_package(package_table.get(2))
+        truck.add_package(package_table.get(33))
+        truck.add_package(package_table.get(10))
+        truck.add_package(package_table.get(5))
+        truck.add_package(package_table.get(38))
+        truck.add_package(package_table.get(8))
         truck.add_package(package_table.get(9))
-        truck.add_package(package_table.get())
+        truck.add_package(package_table.get(3))
 
     return truck
+
+def replace_directions(input: str) -> str:
+    return input.replace('North', 'N').replace('South', 'S').replace('East', 'E').replace('West', 'W')
--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ Fancy schmancy stuff will be abstracted out into separate classes and util files
 from classes.package import Package
 from classes.package_table import Package_Table
 from classes.truck import Truck
-from helpers.helpers import generate_truck
+from helpers.helpers import generate_truck, replace_directions
 import csv
 from pathlib import Path
 
@@ -22,14 +22,20 @@ print("Starting run...\n")
 with open(f"{str(file_path)}/assets/package.csv") as package_file:
     reader = csv.reader(package_file, delimiter=',')
     for row in reader:
-        package = Package(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7] or "")
+        package = Package(row[0], replace_directions(row[1]), row[2], row[3], row[4], row[5], row[6], row[7] or "")
         package_table.put(package)
+
 
 # Generate Mapping for Distances
 # This block of code runs at O(n^2) as it has to go through each row of the 
 # .csv distance file and then again through the list of different addresses
 with open(f"{str(file_path)}/assets/distance.csv") as csvDataFile:
-    # csvDistance = csv.reader(csvDataFile)
     distances = [row for row in csv.reader(csvDataFile)]
+print(distances[0][0])
+print(distances[0][1])
+print(distances[0][2])
 
+print(distances[1][0])
+print(distances[1][1])
+print(distances[1][2])
 print("End of Run.")
